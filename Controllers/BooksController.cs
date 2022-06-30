0ac2937800e81559fb166e831e79ff5512d95d70
Tculls <tanner.culley@gmail.com>
2022-06-30 13:21:20 -0700

    User Routing

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -29,9 +29,4 @@ public class BooksController : Controller
       _context = context;
     }
 
-    [HttpGet("/Books/Dashboard")]
-    public IActionResult Dashboard()
-    {
-      
-    }
 }
--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,6 +1,7 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using BookBevyIII.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace BookBevyIII.Controllers;
 
@@ -28,10 +29,38 @@ public class UsersController : Controller
   {
     _context = context;
   }
+  [HttpGet("/")]
+  public IActionResult LoginAndRegister()
+  {
+    return View("_Register");
+  }
 
   [HttpPost("users/register")]
   public IActionResult Register(User newUser)
   {
+    if (ModelState.IsValid == false)
+    {
+      return LoginAndRegister();
+    }
+    if (_context.Users.Any(User => User.Email == newUser.Email))
+    {
+      ModelState.AddModelError("Email", "is already in use");
+      return LoginAndRegister();
+    }
+        if (_context.Users.Any(User => User.Username == newUser.Username))
+    {
+      ModelState.AddModelError("Username", "is already in use");
+      return LoginAndRegister();
+    }
+    PasswordHasher<User> hashedPW = new PasswordHasher<User>();
+    newUser.Password = hashedPW.HashPassword(newUser, newUser.Password);
+    
+    _context.Users.Add(newUser);
+    _context.SaveChanges();
+
+    HttpContext.Session.SetInt32("UserId", newUser.UserId);
     return View("Book", "Home");
+    
   }
+  
 }
